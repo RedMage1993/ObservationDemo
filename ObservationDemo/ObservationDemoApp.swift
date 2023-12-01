3b28ff795cc5c78a2977a2a3ab69fbf5f398310e
Fritz Ammon <fritz.ammon@slickdeals.net>
2023-12-01 12:12:19 -0800

    Initial Commit

---
+++ ObservationDemo/ObservationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationDemoApp.swift
+//  ObservationDemo
+//
+//  Created by Fritz Ammon on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ObservationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
